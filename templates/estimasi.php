d68b2bc4632132b28acc4fddc8b9a85b3b0a5128
SteveImmanuel <38256664+SteveImmanuel@users.noreply.github.com>
2018-08-10 17:35:42 +0700

    added dropdown menu on estimasi.php

--- templates/estimasi.php
+++ templates/estimasi.php
@@ -7,10 +7,10 @@
     <link rel="stylesheet" type="text/css" href="<?=BASE_URL?>/static/css/queasy.css" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="<?=BASE_URL?>/statis/js/estimasi.js"></script>
+    <script type="text/javascript" src="<?=BASE_URL?>/static/js/estimasi.js"></script>
 </head>
 <body style="background-color:#a3e7ed">
-	
+
 	<nav class="navbar navbar-expand-lg navbar-dark navbar-queasy">
 	    <div class="container" style="margin-left:0px">
 	    	<div class="col-sm-12">
@@ -33,7 +33,23 @@
 			<div style="font-size:90px" id="tulisanantri">01 : 00 : 23</div>
 		</div>
 	</div>
-	
+	<div class="dropdown" id="ddmenu" style="padding-top:60px">
+		<button class="btn btn-outline-light my-2 my-sm-0" type="submit" id="logout-estimasi">LOG OUT</button>
+		<div class="row">
+				<div class="col-sm-1"></div>
+                <i class="fas fa-user-circle" id="userlogo" style="margin-top:0px"></i>
+				<div class="col-sm-8"></div>
+        </div>
+		<div class="row" style="width:100%" >
+                <h4 class="col-sm-2 margin-estimasi">Salma Fedora</h4>
+        </div>
+		<div class="row" style="width:100%">
+                <h4 class="col-sm-1 margin-estimasi">STEI</h4>
+        </div>
+	</div>
+
+	<div class="back-grey" id="bgrey" style="opacity:0.5">
+	</div>
 
 </body>
 </html>
